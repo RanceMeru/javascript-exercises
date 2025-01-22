e7eb1c0aae94c3fcc01b64c5c9233378f1c655e3
RanceMeru <johgu.829@gmail.com>
2025-01-22 10:02:27 -0800

    Javascript code refinement
    gave the if statement brackets to better show what is in the if statement and what is not. I added comments that will better help the user and myself understand what the code does to store for later use.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,19 @@
-    let heyString = `hey`;
-const repeatString = function() {
-    for(i=0; i< 3; i++){
-        return heyString;
-
-    //returned false test failed
+const repeatString = function(word,times) {
+    if(times < 0){
+        
+        return "ERROR";
     }
+    //creates an empty space in between the string outputs
+    let string = "";
+        
+    for(let i=0; i< times; i++){
+        //adds /concatenates the word to the string variable
+        string += word;
+
+        
+    }
+    return string;
+
 };
 
 // Do not edit below this line
