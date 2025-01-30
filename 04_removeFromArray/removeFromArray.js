c0468c00d9b203fe1e00badc632aeb9a218e5765
RanceMeru <johgu.829@gmail.com>
2025-01-30 13:27:27 -0800

    fixed syntax mistakes and changed the for loop to if statement
    fixed some incorrect syntax for the the foreach method and changed the for loop to be an if statement because a loop is not necessary to check if they fufill the direction requirements and instead an if statement will focus the decsions to a boolean true or false to determine what is kept and taken out.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,18 +2,18 @@ const removeFromArray = function(myArray,...args) {
     
     const newArray = [];
 
-    myArray.forEach(number => {
+    myArray.forEach((number) => {
 
         //will be using a for loop to avoid errors occuring in the code
-    for(!args.includes(number)) {
-
+        if(!args.includes(number)) {
         newArray.push(number);
-    }
-        
+        }     
     });
+
+    return newArray;
 
 
     
-}
+};
 // Do not edit below this line
 module.exports = removeFromArray;
