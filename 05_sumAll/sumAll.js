2c9344757a6aa24accb19ce3e5162ee5ab4e3a21
RanceMeru <johgu.829@gmail.com>
2025-02-04 10:27:18 -0800

    Set Up new array and for loops then return the final sum
    Created a new array that can store the final sum of all the integers that will go into it and that they are positive and then to return the final sum.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //will set up a new final sum array
+let finalSum[];
+//will look through an array 
+for (let i = 0; i>0; i++){
+//make sure that the numbers are positive and not odd
+}
+return finalSum();
 
 };
 
