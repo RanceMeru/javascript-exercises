ae546b726b8966614c04b05e95f5b65fdd417127
RanceMeru <johgu.829@gmail.com>
2025-02-04 13:53:52 -0800

    if statements for positive numbers and for loop to increment
    if statements to make sure that num1 and num2 will be larger than 0 and then to see which ones are bigger than the other and then to put it through a loop to then be incremented.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,22 @@
 const sumAll = function(num1, num2) {
-    //will set up a new final sum array
-let finalSum = [];
-//will look through an array 
-for (let i = 0; i>0; i++){
-//make sure that the numbers are positive and not odd
+    
+
+if(num1 <=0 ||um2 <=0){
+    return "Both numbers must be positive";
 }
+
+//make sure we know which is bigger and smaller
+
+const start = Math.min(num1, num2);
+const end = Math.max(num1, num2);
+
+let finalSum = 0;
+for(let i = start; i <= end; i++){
+    sum+=i;
+
+}
+
+//make sure that the numbers are positive and not odd
 return finalSum();
 
 };
