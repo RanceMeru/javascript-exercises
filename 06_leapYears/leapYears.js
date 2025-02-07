ae0f21865f6d525d27728d44f96150bb4e7f9c4f
RanceMeru <johgu.829@gmail.com>
2025-02-07 11:46:02 -0800

    Created new parameters , variables , and conditional if statements
    I created teh new parameter for the leapyears function called givenYear and then created new variables that can be used to shorten and easily use our conditons for leap yeas and then place them in a conditional if statement to see if the given integers are divisible by 4 or400 and false if divisible by 100

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+    //created  new variables that represent our conditions
+    const divideByFour = givenYear % 4 ===0;
+    const divideByHundred = givenYear % 100 === 0;
+    const divideByFourhundred = givenYear % 400 === 0;
+    //returns true if integer is divisible by 4 or 400
+if(divideByFour &&(!divideByHundred || divideByFourhundred)){
+    return true;
 
+}
+else{
+    return false
+}
 };
 
 // Do not edit below this line
