d6dcd483fc108dde51b4874e4f47a81a64d2cbc9
RanceMeru <johgu.829@gmail.com>
2025-02-11 11:31:39 -0800

    Celsius to farenheit and vice versa
    I created a basic formula to calculate the celsius to farenheit and vice versa and then return them.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Farenheit) {
+  const Celsius = ((Farenheit *  9/5) + 32);
+  return Celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  const Farenheit = ((9/5) * Celsius + 32);
+  return Farenheit;
 };
 
 // Do not edit below this line
