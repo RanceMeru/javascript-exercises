b2da4bf00bf8bcfea6ae6944a64551cdd4b908e4
RanceMeru <johgu.829@gmail.com>
2025-02-11 13:06:17 -0800

    fixed formulas and rounded results
    I fixed some errors with the celsius and farenheit conversions and then rounded then using the Mat.round () method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
 const convertToCelsius = function(Farenheit) {
-  const Celsius = ((Farenheit *  9/5) + 32);
-  return Celsius
+  //created the formula to have farenheit converted to celsius and stored in the variable
+  const Celsius = (5/9 *(Farenheit-32));
+  return Math.round(Celsius*10)/10;
 };
 
 const convertToFahrenheit = function(Celsius) {
-  const Farenheit = ((9/5) * Celsius + 32);
-  return Farenheit;
+  const Farenheit = (Celsius * 9/5) + 32; 
+  return Math.round(Farenheit*10)/10;
 };
 
 // Do not edit below this line
