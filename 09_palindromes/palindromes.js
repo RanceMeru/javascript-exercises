8a8b1526dc9f736393092ce423e1367ce3ea649f
RanceMeru <johgu.829@gmail.com>
2025-04-23 11:28:46 -0700

    In the palindromes teset file I removed all of the skip parts of the code so that when I test my code it will not skip the problem that come after the first test. I created a palindromes function that takes in a string then sets a constant called alphanumerical and it will be used to set what we are looking for in our palindrome and then to make a new constant variable called simplifiedString to have it be represented as a string that will be converted to lowercase split into seperare parts then filtered into  using charactert that only use characters from the alphanumerical variable and then it will be joined then a new variable will be set to recombine the string in reverse order then it will be returned.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz123456789';
+
+    const simplifiedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+//need to make it where even with numbers in the string it can be a plaindrome
+const reverseNewString = simplifiedString.split('').reverse().join('');
+
+return simplifiedString === reverseNewString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
