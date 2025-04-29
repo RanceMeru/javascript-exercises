d2b884bc7df96f6147f63f8ce7b54ab794e1cbf6
RanceMeru <johgu.829@gmail.com>
2025-04-29 13:21:01 -0700

    created comments that would help give me clues to what needed to be returned and what method to use in order to retrun only the titles in the array.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(titles) {
+// they will want to have the titles in the array to be returned
+//use the map method
+return titles.map((book)=> book.title)
 };
 
 // Do not edit below this line
