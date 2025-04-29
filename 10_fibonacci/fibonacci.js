ea169f95ea26ec2dd8a32e9cd0508a0685492bd8
RanceMeru <johgu.829@gmail.com>
2025-04-29 09:16:02 -0700

    Created a fibonacci function  to convert input to number then made if statements to hanfle edge cases, initialized an array with the first two fibonacci numbers and made a sequence up to rewquest its index then to return the function.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,25 @@
-const fibonacci = function(n) {
-    
-    
+const fibonacci = function(number) {
+    // Convert input to number
+    let count;
+    if (typeof number !== 'number') {
+        count = parseInt(number);
+    } else {
+        count = number;
+    }
+
+    // Handle edge cases
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    // Initialize array with first two Fibonacci numbers
+    const fib = [0, 1]; 
+
+    // Build sequence up to requested index
+    for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[count];
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
