6ee5c1ca7c57b601644cd73c6b6e7c18fef474b3
RanceMeru <johgu.829@gmail.com>
2025-05-02 11:52:47 -0700

    Removed the skips and added functions to find out whos the oldest
    parts of test so that no parts are skipped and I created a function that takes in death and birth from the arrays and iterateds through them to see if they have a deatah and then sets them as a new object to then subtract death and birth so we can see how old they are.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const getAge = function (death, birth){
 
-};
+if(!death){
+    death = newDate().getFullYear();
+
+}
+return death- birth;
+}
+const findTheOldest = function(people) {
+//can check if someone has a death date or not with an if else statement or a shrotened shorthand version
+//if they do not have a death date then compare who was born first which is less and then display it 2018<2022
+//display the oldest persons name
+//finding out which person/ property in the object is the oldest and display their name 
+//acess an array 
+
+return people.reduce(oldestPerson,currentPerson) => {
+const oldestAge = getAge(Oldest.yearOfBirth, Oldest.yearOfDeath)
+}
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
